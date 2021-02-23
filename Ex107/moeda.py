2d934a93eac58296dae142bf7869cce054dd39f6
João Pedro Fiorelli <joao.fiorelli.13@gmail.com>
2021-02-22 22:53:11 -0300

    Resolução do exercício 107.

---
+++ Ex107/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(n, porcentagem):
+    aum = (1 + porcentagem/100) * n
+    return aum
+
+
+def diminuir(n, porcentagem):
+    dim = (1 - porcentagem/100) * n
+    return dim
+
+
+def dobro(n):
+    dob = 2 * n
+    return dob
+
+
+def metade(n):
+    met = n / 2
+    return met
